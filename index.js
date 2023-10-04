4d0fd782d45ab3e26014e8082d81c7decc39215a
M7Pedroso <mb.pedroso7@gmail.com>
2023-10-03 23:32:33 -0300

    Meu Segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-asfasfasfas
+let nomeDoHeroi = "Zeus"
+let xpDoHeroi = "9000"
+//let eloDoHeroi = ""
+
+if (xpDoHeroi < 1000){
+    console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de Ferro")
+}
+else if (
+    xpDoHeroi > 1000 &&
+    xpDoHeroi <= 2000
+    
+    ){
+    console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de Bronze")
+}
+else if (
+    xpDoHeroi > 2000 &&
+    xpDoHeroi <= 5000
+    
+    ){
+    console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de Prata")
+}
+else if (
+    xpDoHeroi > 6000 &&
+    xpDoHeroi <= 7000
+    
+){
+
+    console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de Ouro")
+}
+else if (
+    xpDoHeroi > 7001 &&
+    xpDoHeroi <= 8000
+    
+){
+    console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de Platina")
+}
+else if (
+    xpDoHeroi > 8001 &&
+    xpDoHeroi <= 9000
+    
+
+){
+    console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de ASCendenTe")
+}else if (
+    xpDoHeroi > 9001 &&
+    xpDoHeroi <= 10000
+    
+){
+    console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de IMORTAL")
+}
+else if (
+    xpDoHeroi > 10001
+
+){
+    console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível RADIANTE")
+}
