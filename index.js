603f669ae702ae68d7a246e5afe88e08fe935999
M7Pedroso <mb.pedroso7@gmail.com>
2023-10-04 22:28:41 -0300

    Meu terceiro commit

--- index.js
+++ index.js
@@ -5,28 +5,27 @@ let xpDoHeroi = "9000"
 if (xpDoHeroi < 1000){
     console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de Ferro")
 }
+
 else if (
     xpDoHeroi > 1000 &&
     xpDoHeroi <= 2000
-    
     ){
     console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de Bronze")
 }
+
 else if (
     xpDoHeroi > 2000 &&
     xpDoHeroi <= 5000
-    
     ){
     console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de Prata")
 }
 else if (
     xpDoHeroi > 6000 &&
     xpDoHeroi <= 7000
-    
 ){
-
     console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de Ouro")
 }
+
 else if (
     xpDoHeroi > 7001 &&
     xpDoHeroi <= 8000
@@ -37,20 +36,18 @@ else if (
 else if (
     xpDoHeroi > 8001 &&
     xpDoHeroi <= 9000
-    
-
 ){
     console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de ASCendenTe")
-}else if (
+}
+
+else if (
     xpDoHeroi > 9001 &&
     xpDoHeroi <= 10000
-    
 ){
     console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível de IMORTAL")
 }
-else if (
-    xpDoHeroi > 10001
 
-){
+else if (
+    xpDoHeroi > 10001){
     console.log("O Herói de nome " + nomeDoHeroi +  ", está no nível RADIANTE")
 }
